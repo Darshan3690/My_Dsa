045c117eeceeb0c5b36cb2dac61a752798f3e1d8
Darshan Rajput <darshan.rajput123091@marwadiuniversity.ac.in>
2026-07-23 14:34:06 +0530

    Time: 0 ms (100%), Space: 42.1 MB (33.21%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,13 +1,17 @@
 class Solution {
     public int fib(int n) {
 
-        if(n==1){
-            return 1;
-        }
-        if(n==0){
-            return 0;
-        }
+       if(n<=1) return n;
 
-        return fib(n-1)+fib(n-2);      
+        int prev2=0,prev1 = 1;
+
+        for(int i=2;i<=n;i++){
+
+            int curri = prev1+prev2;
+            prev2 = prev1;
+            prev1= curri;
+        }
+        return prev1;
+
     }
 }
