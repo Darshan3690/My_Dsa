22d879865bdea5d49b23fd4e2cbbdc0709326b64
Darshan Rajput <darshan.rajput123091@marwadiuniversity.ac.in>
2026-08-10 13:16:12 +0530

    Time: 0 ms (100%), Space: 44 MB (24.7%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean searchMatrix(int[][] mat, int target) {
+
+        
+
+
+        
+
+        boolean found = false;
+        int n=mat.length , m=mat[0].length , l=0,rig=n*m-1;
+
+        while(l<=rig){
+            int mid=l+(rig-l)/2;
+
+          int r=mid/m;
+          int c=mid%m;
+
+          if(mat[r][c]==target){
+            return true;
+          }
+          if(mat[r][c]<target){
+            l=mid+1;
+          }
+          else{
+            rig=mid-1;
+          }
+        }
+
+        return false;
+       
+   
+        
+ 
+
+    }}
