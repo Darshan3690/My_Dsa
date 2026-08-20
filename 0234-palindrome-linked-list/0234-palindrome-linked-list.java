3ce1eec371bb449b0d6483459477a884007f2a1e
Darshan Rajput <darshan.rajput123091@marwadiuniversity.ac.in>
2026-08-20 11:49:04 +0530

    Time: 5 ms (42.43%), Space: 94.7 MB (36.65%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -1,13 +1,11 @@
 class Solution {
 
     ListNode reverse(ListNode head) {
-
         ListNode pre = null;
         ListNode curr = head;
 
         while (curr != null) {
             ListNode next1 = curr.next;
-
             curr.next = pre;
             pre = curr;
             curr = next1;
@@ -21,21 +19,15 @@ class Solution {
         ListNode slow = head;
         ListNode fast = head;
 
-        // Find middle
         while (fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
         }
 
-        // Reverse second half
         ListNode secondh = reverse(slow);
-
-        // Start from beginning
         ListNode first = head;
 
-        // Compare both halves
         while (secondh != null) {
-
             if (first.val != secondh.val) {
                 return false;
             }
